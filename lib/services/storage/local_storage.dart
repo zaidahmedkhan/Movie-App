4d8e8ba125a6d4bffc42968d0431e1ef7229feb4
ZaidAhmedKhan-20 <zaid.ahmed@byklabs.com>
2025-09-22 18:28:36 +0500

    secure storage package

---
+++ lib/services/storage/local_storage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LocalStorage {
+  final storage = const FlutterSecureStorage();
+
+  Future<bool> setKey(String key, String value) async {
+    await storage.write(key: key, value: value);
+    return true;
+  }
+
+  Future<dynamic> readValue(String key) async {
+    await storage.read(key: key);
+  }
+
+  Future<bool> clearValue(String key) async {
+    await storage.delete(key: key);
+    return true;
+  }
+}
